4a34b9697118fdf2e9d7d247ca0476e47e53eaf6
Travis Briggs <audiodude@gmail.com>
2023-09-14 09:45:13 -0700

    Script for generating and uploading finetuning data

---
+++ finetune.py
@@ -0,0 +1,60 @@
+import json
+import os
+import sys
+
+import requests
+
+import parse_emails
+
+OPENAPI_TOKEN = os.getenv('OPENAPI_TOKEN')
+
+
+def prepare_input_messages(emails):
+  return [
+      json.dumps({
+          'messages': [{
+              'role':
+                  'system',
+              'content':
+                  'You are an assistant that writes short, sweet love letters from Travis to Abby'
+          }, {
+              'role':
+                  'user',
+              'content':
+                  'Write me a love letter from myself, Travis, that I can send to my wife Abby'
+          }, {
+              'role': 'assistant',
+              'content': email,
+          }]
+      }) for email in emails
+  ]
+
+
+def upload_fine_tune_file():
+  with open('finetune.jsonl', 'w') as f:
+    f.write('\n'.join(prepare_input_messages(parse_emails.get_email_texts())))
+
+  headers = {'Authorization': 'Bearer %s' % OPENAPI_TOKEN}
+  files = {'file': open('finetune.jsonl', 'rb')}
+  r = requests.post('https://api.openai.com/v1/files',
+                    headers=headers,
+                    files=files,
+                    data={'purpose': 'fine-tune'})
+  data = r.json()
+  return data['id']
+
+
+if not os.path.exists('finetune.jsonl'):
+  file_id = upload_fine_tune_file()
+else:
+  file_id = sys.argv[1]
+
+fine_tune_job = {'training_file': file_id, 'model': 'gpt-3.5-turbo-0613'}
+headers = {
+    'Authorization': 'Bearer %s' % OPENAPI_TOKEN,
+    'Content-Type': 'application/json'
+}
+r = requests.post('https://api.openai.com/v1/fine_tuning/jobs',
+              headers=headers,
+              json=fine_tune_job)
+print(r.text)
