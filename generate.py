2a365bb73c1d40dcdb6c3885fe4c5028de124200
Travis Briggs <audiodude@gmail.com>
2023-09-14 09:52:57 -0700

    Add script for generating letters

---
+++ generate.py
@@ -0,0 +1,36 @@
+import json
+import os
+import sys
+
+import requests
+
+OPENAPI_TOKEN = os.getenv('OPENAPI_TOKEN')
+
+
+def generate():
+  data = {
+      'model': 'ft:gpt-3.5-turbo-0613:personal::7ybccOXf',
+      'messages': [{
+          'role':
+              'system',
+          'content':
+              'You are an assistant that writes whort, sweet love letters from Travis to Abby'
+      }, {
+          'role':
+              'user',
+          'content':
+              'Write me a love letter from myself, Travis, that I can send to my wife Abby'
+      }],
+      'stop': 'Love Forever,\n-Travis',
+  }
+
+  r = requests.post('https://api.openai.com/v1/chat/completions', headers={'Authorization': 'Bearer %s' % OPENAPI_TOKEN}, json=data)
+  output = r.json()
+  try:
+    return output['choices'][0]['message']['content'] + 'Love Forever,\n-Travis'
+  except KeyError:
+    print(output)
+
+if __name__ == '__main__':
+  letter = generate()
+  print(letter)
