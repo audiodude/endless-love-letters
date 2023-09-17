d9d6e15adf73286cb8087bdc5b320e261008460c
Travis Briggs <audiodude@gmail.com>
2023-09-17 12:44:58 -0700

    Use petite-vue to load letter

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import flask
 
 app = flask.Flask(__name__)
 
+
 @app.route('/')
 def index():
   return flask.render_template('index.html')
+
 
 @app.route('/<version>')
 def view(version):
@@ -12,12 +14,14 @@ def view(version):
     flask.abort(404)
   return flask.render_template(f'{version}/view.html')
 
+
 @app.route('/<version>/favorites')
 def favorites(version):
   if version not in ('v1', 'v2'):
     flask.abort(404)
   return flask.render_template(f'{version}/favorites.html')
 
+
 @app.route('/api/generate')
 def generate_letter():
-  return flask.jsonify(['Foo test letter'])
+  return flask.jsonify({'letter': 'Foo test letter'})
