74145b2db99ab2d604c9d060e2499da4e10510a4
Travis Briggs <audiodude@gmail.com>
2023-10-16 23:00:34 -0700

    More debugging

--- get_secrets.py
+++ get_secrets.py
@@ -2,12 +2,12 @@ import os
 
 import dotenv
 
-
 dotenv.load_dotenv()
 
 
 def secret(name):
   val = os.environ.get(name)
+  print(f'{name}={val}')
   if name == 'MYSQL_PORT':
     return val and int(val)
   return val
