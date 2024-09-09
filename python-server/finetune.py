2c84655c51f10c6a9ef146420d2569715f8662cd
Travis Briggs <audiodude@gmail.com>
2024-09-08 19:31:58 -0700

    Refactor of finetune.py

--- python-server/finetune.py
+++ python-server/finetune.py
@@ -30,10 +30,12 @@ def prepare_input_messages(emails):
   ]
 
 
-def upload_fine_tune_file():
+def write_finetune_file():
   with open('finetune.jsonl', 'w') as f:
     f.write('\n'.join(prepare_input_messages(parse_emails.get_email_texts())))
 
+
+def upload_fine_tune_file():
   headers = {'Authorization': 'Bearer %s' % OPENAPI_TOKEN}
   files = {'file': open('finetune.jsonl', 'rb')}
   r = requests.post('https://api.openai.com/v1/files',
@@ -44,17 +46,24 @@ def upload_fine_tune_file():
   return data['id']
 
 
-if not os.path.exists('finetune.jsonl'):
-  file_id = upload_fine_tune_file()
-else:
-  file_id = sys.argv[1]
+def start_finetune_job():
+  fine_tune_job = {'training_file': file_id, 'model': 'gpt-3.5-turbo-0613'}
+  headers = {
+      'Authorization': 'Bearer %s' % OPENAPI_TOKEN,
+      'Content-Type': 'application/json'
+  }
+  r = requests.post('https://api.openai.com/v1/fine_tuning/jobs',
+                    headers=headers,
+                    json=fine_tune_job)
 
-fine_tune_job = {'training_file': file_id, 'model': 'gpt-3.5-turbo-0613'}
-headers = {
-    'Authorization': 'Bearer %s' % OPENAPI_TOKEN,
-    'Content-Type': 'application/json'
-}
-r = requests.post('https://api.openai.com/v1/fine_tuning/jobs',
-              headers=headers,
-              json=fine_tune_job)
-print(r.text)
+  return r.text
+
+
+if __name__ == '__main__':
+  if not os.path.exists('finetune.jsonl'):
+    write_finetune_file()
+    file_id = upload_fine_tune_file()
+  else:
+    file_id = sys.argv[1]
+
+  print(start_finetune_job())
