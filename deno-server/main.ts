08ddbab902a843467e44f8f5baddf2ed54f8dfd9
Travis Briggs <audiodude@gmail.com>
2024-09-08 19:31:42 -0700

    Rename js-server -> deno-server

--- deno-server/main.ts
+++ deno-server/main.ts
@@ -1,5 +1,5 @@
 // @deno-types="npm:@types/express@4"
-import express, { Request, Response } from 'npm:express@4.19.2';
+import express, { Request, Response } from 'npm:express@4';
 
 const app = express();
 const port = Number(Deno.env.get('PORT')) || 3000;
